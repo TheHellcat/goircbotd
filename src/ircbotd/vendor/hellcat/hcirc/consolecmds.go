f4d7a06e9fac21a0d0d68c4db099e3de6ef6e7e7
Hellcat <hellcat@hellcat.net>
2016-05-13 11:31:12 +0200

    few more simple console commands

--- src/ircbotd/vendor/hellcat/hcirc/consolecmds.go
+++ src/ircbotd/vendor/hellcat/hcirc/consolecmds.go
@@ -6,7 +6,7 @@ import (
 )
 
 
-func (hcIrc *HcIrc) conscmdSay( params string ) string {
+func (hcIrc *HcIrc) conscmdSay( command, params string ) string {
     var a []string
     var s string
     var r string
@@ -16,13 +16,37 @@ func (hcIrc *HcIrc) conscmdSay( params string ) string {
         hcIrc.OutboundQueue <- s
         r = fmt.Sprintf( "Sent text to %s\n", a[0] )
     } else {
-        r = fmt.Sprintf( "Not enough parameters, usage: say <#channel> <text>" )
+        r = fmt.Sprintf( "Not enough parameters, usage: say <#channel> <text>\n" )
     }
 
     return r
 }
 
 
+func (hcIrc *HcIrc) conscmdChannels( command, params string ) string {
+    return ""
+}
+
+
+func (hcIrc *HcIrc) conscmdJoin( command, params string ) string {
+    var s string
+    s = fmt.Sprintf( "JOIN %s", params )
+    hcIrc.OutQuickQueue <- s
+    return ""
+}
+
+
+func (hcIrc *HcIrc) conscmdPart( command, params string ) string {
+    var s string
+    s = fmt.Sprintf( "PART %s", params )
+    hcIrc.OutQuickQueue <- s
+    return ""
+}
+
+
 func (hcIrc *HcIrc) RegisterAdditionalConsoleCommands() {
     hcIrc.RegisterConsoleCommand( "say", "Says something into the given channel", hcIrc.conscmdSay )
+    hcIrc.RegisterConsoleCommand( "channels", "Lists all channels the bot is joined into", hcIrc.conscmdChannels )
+    hcIrc.RegisterConsoleCommand( "join", "Joins a channel", hcIrc.conscmdJoin )
+    hcIrc.RegisterConsoleCommand( "part", "Parts a channel", hcIrc.conscmdPart )
 }
