1e587ecc9989966bef407a2b6159429906c7d4cc
Hellcat <hellcat@hellcat.net>
2016-05-18 11:30:12 +0200

    actual implementation of websocket handler and webchat handling

--- src/ircbotd/internal/ircbotint/webchat.go
+++ src/ircbotd/internal/ircbotint/webchat.go
@@ -18,6 +18,9 @@ var wsUpgrader = websocket.Upgrader{
 var wsHcIrc *hcirc.HcIrc
 
 
+/**
+ * check the origin of the request if we want to allow the connection or not
+ */
 func checkOrigin( r *http.Request ) bool {
     if "http://live.hellcat.net" == r.Header.Get("Origin") {
         return true
@@ -27,47 +30,121 @@ func checkOrigin( r *http.Request ) bool {
 }
 
 
-func test(w http.ResponseWriter, r *http.Request) {
-    var s string
-    var msgChan chan string
+/**
+ * client receiver thrread
+ *
+ * waits for messages being sent from the webclient and sends them to the request handler
+ */
+func clientReceiver( conn *websocket.Conn, inChan chan string ) {
+    var mt int
+    var ba []byte
+    var message string
+    var err error
+    var running bool
 
-    fmt.Println( "WS TEST HANDLER" )
+    running = true
 
-    msgChan = make(chan string)
-    s = fmt.Sprintf( "swtest-%s-%s", r.RemoteAddr, time.Now().String() )
-    fmt.Println( s )
-    wsHcIrc.RegisterServerMessageHook( s, msgChan )
-
-    c, err := wsUpgrader.Upgrade(w, r, nil)
-    if err != nil {
-        // TODO: Handle error
-        fmt.Println(err.Error())
-        return
+    if wsHcIrc.Debugmode {
+        fmt.Printf( "[WSDEBUG] Receiver thread started\n" )
     }
-    defer c.Close()
-    //for {
-        mt, message, err := c.ReadMessage()
-    message = message
+
+    for running {
+        mt, ba, err = conn.ReadMessage()
         if err != nil {
-            // TODO: Handle error
-            //break
-            return
-        }
-        fmt.Println( mt )
-        for {
-            s = <-msgChan
-            //fmt.Printf("** TEST ** ws-recv: %s\n", s)
-            err = c.WriteMessage(mt, []byte(s))
-            if err != nil {
-                // TODO: Handle error
-                break
+            if wsHcIrc.Debugmode {
+                fmt.Printf( "[WSDEBUG] Feiled to read from connection: %s\n", err.Error() )
+            }
+            inChan <- "QUIT"
+            running = false
+        } else {
+            message = string(ba)
+            if mt == websocket.TextMessage {
+                inChan <- message
             }
         }
-    //}
+    }
+
+    if wsHcIrc.Debugmode {
+        fmt.Printf( "[WSDEBUG] Receiver thread ended\n" )
+    }
 }
+
+
+/**
+ * main request handler
+ *
+ * handles the HTTP request, upgrade to WEBSOCKET and communication between bot and webclient
+ */
+func webchatHandler (writer http.ResponseWriter, request *http.Request) {
+    var s string
+    var msgChan chan string
+    var inChan chan string
+    var conn *websocket.Conn
+    var err error
+    var running bool
+    var command, channel, nick, text string
+    var myId string
+
+    running = true
+    myId = request.RemoteAddr
+
+    if wsHcIrc.Debugmode {
+        fmt.Printf( "[WSDEBUG] New connection handler spawned: %s\n", myId )
+    }
+
+    // setup new channel to receive IRC server messages
+    msgChan = make(chan string, wsHcIrc.QueueSize)
+    // we need a unique ID for registering our channel
+    s = fmt.Sprintf( "webchat-%s-%d", request.RemoteAddr, time.Now().Unix() )
+    wsHcIrc.RegisterServerMessageHook( s, msgChan )
+    if wsHcIrc.Debugmode {
+        fmt.Printf( "[WSDEBUG] Registered server-messages channel with ID %s\n", s )
+    }
+
+    // set up channel for receiving messages from webchat client
+    inChan = make(chan string, wsHcIrc.QueueSize)
+
+    // upgrade the HTTP connection to WEBSOCKETS
+    conn, err = wsUpgrader.Upgrade(writer, request, nil)
+    if err != nil {
+        if wsHcIrc.Debugmode {
+            fmt.Printf( "[WSDEBUG] Upgrading HTTP to WEBSOCKETS feiled: %s\n", err.Error() )
+        }
+        return
+    }
+    defer conn.Close()
+
+    // fork out the reader as separate routine/thread and listen on a chan
+    // for it, this way we can have the read non-blocking and react on other
+    // things as well while waiting for the client to send something
+    go clientReceiver(conn, inChan)
+
+    for running {
+        select {
+        case s = <-inChan:
+            if "QUIT" == s {
+                running = false
+            }
+        case s = <-msgChan:
+            command, channel, nick, _, _, text = wsHcIrc.ParseMessage(s)  // TODO: optimize this to not have every handler call Parse() for the same message
+            if "PRIVMSG" == command {
+                s = fmt.Sprintf( "[%s] %s: %s", channel, nick, text )
+                err = conn.WriteMessage(websocket.TextMessage, []byte(s))
+                if err != nil {
+                    // TODO: Handle error
+                }
+            }
+        }
+    }
+
+    if wsHcIrc.Debugmode {
+        fmt.Printf( "[WSDEBUG] Connection handler terminated: %s\n", myId )
+    }
+}
+
 
 func TestWs(hcIrc *hcirc.HcIrc) {
     wsHcIrc = hcIrc
-    http.HandleFunc("/test", test)
-    http.ListenAndServe("0.0.0.0:1234", nil)
+    http.HandleFunc("/webchat", webchatHandler)
+    http.ListenAndServe("0.0.0.0:8088", nil)
 }
