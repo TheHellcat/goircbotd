8a914cc4135ae3634111368fd22e766b3595cd4b
Hellcat <hellcat@hellcat.net>
2017-01-31 11:07:07 +0100

    very small text change

--- src/ircbotd/vendor/hellcat/hcirc/hcirc.go
+++ src/ircbotd/vendor/hellcat/hcirc/hcirc.go
@@ -146,7 +146,7 @@ func (hcIrc *HcIrc) SetRealname(name string) {
  */
 func (hcIrc *HcIrc) EnableTwitchMode() {
     hcIrc.twitchMode = true
-    fmt.Printf("Running in Twitch mode.\n")
+    fmt.Printf("(i) Running in Twitch mode!\n")
     hcIrc.debugPrint("Enabling Twitch compatibility mode", "")
 }
 
